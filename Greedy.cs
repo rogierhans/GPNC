046e588fa935646a7de8ec5868c0909fadcbabbc
rogierhans <rogierhans@gmail.com>
2017-02-20 23:40:28 +0100

    O(n^2)- > O(n log n)

--- Greedy.cs
+++ Greedy.cs
@@ -17,19 +17,16 @@ namespace GPNC
         public static void mergeGraph(Graph G, int U)
         {
 
-            Random r = new Random();
-            Dictionary<Edge, double> scores = new Dictionary<Edge, double>();
+            EdgeStructure ES = new EdgeStructure(U);
             G.GetAllArcs(U).ForEach(e =>
             {
-                int u = e.To;
-                int v = e.From;
-                scores[e] = randomNumber(r) * ((G.getWeight(u, v) / Math.Sqrt(G.Size[u])) + (G.getWeight(u, v) / Math.Sqrt(G.Size[v])));
+                ES.AddEdge(G, e);
             });
 
-            while (scores.Count > 0)
+            while (ES.NonEmpty())
             {
                 //O(n) -> alleen neighbours
-                Edge bestEdge = getBestEdge(scores);
+                Edge bestEdge = ES.BestEdge();
                 int newID = bestEdge.From;
                 int deletedId = bestEdge.To;
                 List<int> neighbours = G.GetNeighbours(deletedId);
@@ -37,28 +34,28 @@ namespace GPNC
                 //Console.WriteLine("u:{0}::{1} - v:{2}::{3}", bestEdge.From, G.Size[bestEdge.From], bestEdge.To, G.Size[bestEdge.To]);
 
                 G.Contraction(newID, deletedId);
-                if (!scores.Remove(bestEdge)) Console.WriteLine("HDNW");
+                //ES.RemoveEdge(bestEdge);
 
                 //Change edges
                 foreach (int neighbour in neighbours)
                 {
                     Edge oldEdge = new Edge(deletedId, neighbour);
                     Edge newEdge = new Edge(newID, neighbour);
-                    if (scores.ContainsKey(oldEdge))
-                    {
-                        scores.Remove(oldEdge);
-                        AddEdge(G, scores, U, newEdge, r);
 
+                    if (ES.Contains(oldEdge))
+                    {
+                        ES.RemoveEdge(oldEdge);
+                        ES.AddEdge(G, newEdge);
                     }
                 }
                 //remove edges that have become to big  
                 foreach (int neighbour in neighboursND)
                 {
                     Edge newEdge = new Edge(newID, neighbour);
-                    if (scores.ContainsKey(newEdge))
+                    if (ES.Contains(newEdge))
                     {
                         if (G.Size[newEdge.To] + G.Size[newEdge.From] >= U)
-                            scores.Remove(newEdge);
+                            ES.RemoveEdge(newEdge);
                     }
                 }
 
@@ -68,7 +65,7 @@ namespace GPNC
 
 
 
-        private static void AddEdge(Graph G, Dictionary<Edge, double> scores, int U, Edge e,Random r)
+        private static void AddEdge(Graph G, Dictionary<Edge, double> scores, int U, Edge e, Random r)
         {
             if (G.Size[e.To] + G.Size[e.From] < U)
             {
@@ -93,6 +90,8 @@ namespace GPNC
             return bestEdge;
         }
 
+
+
         private static double randomNumber(Random r)
         {
             double a = 0.03;
@@ -108,7 +107,55 @@ namespace GPNC
                 return (r.NextDouble() * (1 - b)) + b;
             }
         }
+        class EdgeStructure
+        {
 
-
+            Dictionary<Edge, double> scores = new Dictionary<Edge, double>();
+            private SortedSet<Edge> sortedEdges;
+            Random r = new Random();
+            int U;
+            public EdgeStructure(int u)
+            {
+                U = u;
+                sortedEdges = new SortedSet<Edge>(new EdgeCompare(scores));
+            }
+            public void RemoveEdge(Edge e)
+            {
+                sortedEdges.Remove(e);
+                scores.Remove(e);
+                //Console.WriteLine("Remove {0} score:{1} set{2}", e, scores.Count, sortedEdges.Count);
+            }
+            public void AddEdge(Graph G, Edge e)
+            {
+                if (Contains(e)) RemoveEdge(e);
+                if (G.Size[e.To] + G.Size[e.From] < U)
+                {
+                    int u = e.To;
+                    int v = e.From;
+                    scores[e] = randomNumber(r) * ((G.getWeight(u, v) / Math.Sqrt(G.Size[u])) + (G.getWeight(u, v) / Math.Sqrt(G.Size[v])));
+                    sortedEdges.Add(e);
+                }
+                //Console.WriteLine("Add {0} score:{1} set{2}", e, scores.Count, sortedEdges.Count);
+            }
+            public bool Contains(Edge e) { return scores.ContainsKey(e); }
+            public Edge BestEdge()
+            {
+                Edge e = sortedEdges.Max;
+                RemoveEdge(e);
+                return e;
+            }
+            public bool NonEmpty() { return scores.Count > 0; }
+        }
+        class EdgeCompare : IComparer<Edge>
+        {
+            Dictionary<Edge, double> scores;
+            public EdgeCompare(Dictionary<Edge, double> scores) { this.scores = scores; }
+            public int Compare(Edge x, Edge y)
+            {
+                return scores[x].CompareTo(scores[y]);
+            }
+        }
     }
+
+
 }
